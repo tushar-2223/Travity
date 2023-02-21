218ab9c5d928434133606a04449a455b80b2b065
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2023-02-21 16:38:18 +0530

    add motion

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -10,9 +10,12 @@ export const Packages = () => {
             <div className='flex justify-center items-center px-2 md:px-0'>
                 <div className='bg-white/90 rounded-xl border-2'>
                     <div className='flex flex-wrap py-5 px-5'>
+
                         <div className='w-full md:w-[400px] md:pr-5'>
-                            <div className='bg-delhi bg-cover w-full h-[30rem] rounded-xl mb-5 p-2 bg-blue-600/10 overflow-hidden hover:border-4 hover:border-gray-900'>
-                                <div className='h-full w-full p-7 flex items-end justify-between'>
+                            <div className='relative w-full h-[30rem] rounded-xl mb-5 bg-blue-600/10 overflow-hidden'>
+
+
+                                <div className='absolute w-full bottom-0 p-7 flex items-end justify-between z-10'>
                                     <div>
                                         <h1 className='text-6xl font-bold text-white'>Delhi</h1>
                                         <h4 className='text-blue-500 font-bold text-2xl'>5 Day</h4>
@@ -21,10 +24,15 @@ export const Packages = () => {
                                         <button className='p-3 bg-blue-300 rounded-full'><ArrowOutwardIcon /></button>
                                     </div>
                                 </div>
+
+                                <div className='h-full w-full bg-delhi bg-cover hover:scale-125 transition delay-50 ease-in-out'></div>
+
                             </div>
 
-                            <div className='bg-assam bg-cover w-full h-[20rem] rounded-xl mb-5 p-2 bg-blue-600/10 overflow-hidden hover:border-4 hover:border-gray-900'>
-                                <div className='h-full w-full p-7 flex items-end justify-between'>
+
+                            <div className='relative w-full h-[20rem] rounded-xl mb-5 bg-blue-600/10 overflow-hidden'>
+
+                                <div className='absolute w-full bottom-0 p-7 flex items-end justify-between z-10'>
                                     <div>
                                         <h1 className='text-6xl font-bold text-white'>Assam</h1>
                                         <h4 className='text-blue-500 font-bold text-2xl'>5 Day</h4>
@@ -33,14 +41,19 @@ export const Packages = () => {
                                         <button className='p-3 bg-blue-300 rounded-full'><ArrowOutwardIcon /></button>
                                     </div>
                                 </div>
+
+                                <div className='h-full w-full bg-assam bg-cover hover:scale-125 transition delay-50 ease-in-out'></div>
+
                             </div>
                         </div>
 
 
                         <div className='w-full md:w-[400px] md:pr-5'>
 
-                            <div className='bg-himalya bg-cover w-full h-[20rem] rounded-xl mb-5 p-2 bg-blue-600/10 overflow-hidden hover:border-4 hover:border-gray-900'>
-                                <div className='h-full w-full p-7 flex items-end justify-between'>
+                            <div className='relative w-full h-[20rem] rounded-xl mb-5 bg-blue-600/10 overflow-hidden'>
+
+
+                                <div className='absolute w-full bottom-0 p-7 flex items-end justify-between z-10'>
                                     <div>
                                         <h1 className='text-6xl font-bold text-white'>Himalya</h1>
                                         <h4 className='text-blue-500 font-bold text-2xl'>5 Day</h4>
@@ -49,11 +62,17 @@ export const Packages = () => {
                                         <button className='p-3 bg-blue-300 rounded-full'><ArrowOutwardIcon /></button>
                                     </div>
                                 </div>
+
+                                <div className='h-full w-full bg-himalya bg-cover hover:scale-125 transition delay-50 ease-in-out'></div>
+
                             </div>
 
 
-                            <div className='bg-goa bg-cover w-full h-[30rem] rounded-xl mb-5 p-2 bg-blue-600/10 overflow-hidden hover:border-4 hover:border-gray-900'>
-                                <div className='h-full w-full p-7 flex items-end justify-between'>
+
+                            <div className='relative w-full h-[30rem] rounded-xl mb-5 bg-blue-600/10 overflow-hidden'>
+
+
+                                <div className='absolute w-full bottom-0 p-7 flex items-end justify-between z-10'>
                                     <div>
                                         <h1 className='text-6xl font-bold text-white'>Goa</h1>
                                         <h4 className='text-blue-500 font-bold text-2xl'>5 Day</h4>
@@ -62,23 +81,32 @@ export const Packages = () => {
                                         <button className='p-3 bg-blue-300 rounded-full'><ArrowOutwardIcon /></button>
                                     </div>
                                 </div>
+
+                                <div className='h-full w-full bg-goa bg-cover hover:scale-125 transition delay-50 ease-in-out'></div>
+
                             </div>
 
                         </div>
 
                         <div className='w-full md:w-[400px]'>
 
-                            <div className='bg-gujarat bg-cover w-full h-[51rem] rounded-xl mb-5 p-2 bg-blue-600/10 overflow-hidden hover:border-4 hover:border-gray-900'>
-                                <div className='h-full w-full p-7 flex items-end justify-between'>
+                            <div className='relative w-full h-[51rem] rounded-xl mb-5 bg-blue-600/10 overflow-hidden'>
+                            
+                                
+                                 <div className='absolute w-full bottom-0 p-7 flex items-end justify-between z-10'>
                                     <div>
                                         <h1 className='text-6xl font-bold text-white'>Gujarat</h1>
-                                        <h4 className='text-blue-500 font-bold text-2xl'>10 Day</h4>
+                                        <h4 className='text-blue-500 font-bold text-2xl'>5 Day</h4>
                                     </div>
                                     <div>
                                         <button className='p-3 bg-blue-300 rounded-full'><ArrowOutwardIcon /></button>
                                     </div>
                                 </div>
+
+                                <div className='h-full w-full bg-gujarat bg-cover hover:scale-125 transition delay-50 ease-in-out'></div>
+
                             </div>
+
 
 
                         </div>
